8e93d751b908d4ea6e704eab8d77f346330fc4e7
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2025-03-12 13:04:35 +0100

    init: initialize Angular application
    Set up a new Angular project with default configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonComponent} from './button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'eshop';
+  inCart = 0;
+
+  addToCart() {
+    this.inCart += 1;
+  }
+
+  removeFromCart() {
+    this.inCart -= 1;
+  }
 }
